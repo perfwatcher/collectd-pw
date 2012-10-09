95ceb4d5796d9e84f1bd738dab3069fca7d6d635
Cyril Feraudet <cyril@feraudet.com>
2012-10-09 13:16:27 +0200

    Revert "Try again to fix notification forwarding"
    This reverts commit a18b62f687710658248fd20539d3755bae86b04e.
    
    Conflicts:
    
    	src/network.c

--- src/network.c
+++ src/network.c
@@ -370,9 +370,6 @@ static _Bool check_send_notify_okay (const notification_t *n) /* {{{ */
   static c_complain_t complain_forwarding = C_COMPLAIN_INIT_STATIC;
   _Bool received = 0;
 
-  /* Enable notification forwarding */
-  if (network_config_forward) { return (0); }
-
   if (n->meta == NULL)
     return (1);
 
