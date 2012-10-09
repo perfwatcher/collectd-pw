e3490cfdf6e1e64138b83bd5044f0718f43ffb1d
Cyril Feraudet <cyril@feraudet.com>
2012-10-09 14:08:39 +0200

    Definitive patch for notification forwarding

--- src/network.c
+++ src/network.c
@@ -370,11 +370,6 @@ static _Bool check_send_notify_okay (const notification_t *n) /* {{{ */
   static c_complain_t complain_forwarding = C_COMPLAIN_INIT_STATIC;
   _Bool received = 0;
 
-  /* Enable notification forwarding */
-  if (!network_config_forward) { return (0); }
-  return (1);
-  /* Enable notification forwarding */
-
   if (n->meta == NULL)
     return (1);
 
@@ -382,13 +377,13 @@ static _Bool check_send_notify_okay (const notification_t *n) /* {{{ */
 
   if (network_config_forward && received)
   {
-    c_complain_once (LOG_ERR, &complain_forwarding,
+    /*c_complain_once (LOG_ERR, &complain_forwarding,
         "network plugin: A notification has been received via the network "
         "forwarding if enabled. Forwarding of notifications is currently "
         "not supported, because there is not loop-deteciton available. "
         "Please contact the collectd mailing list if you need this "
-        "feature.");
-		return (1);
+        "feature.");*/
+	return (1);
   }
 
   /* By default, only *send* value lists that were not *received* by the
