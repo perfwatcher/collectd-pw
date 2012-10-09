a372fd70bce7b4b25c3e53a27f7d3eea4465706a
Cyril Feraudet <cyril@feraudet.com>
2012-10-09 14:14:10 +0200

    Removing unused variable

--- src/network.c
+++ src/network.c
@@ -367,7 +367,9 @@ static _Bool check_notify_received (const notification_t *n) /* {{{ */
 
 static _Bool check_send_notify_okay (const notification_t *n) /* {{{ */
 {
+  /*
   static c_complain_t complain_forwarding = C_COMPLAIN_INIT_STATIC;
+  */
   _Bool received = 0;
 
   if (n->meta == NULL)
@@ -377,12 +379,14 @@ static _Bool check_send_notify_okay (const notification_t *n) /* {{{ */
 
   if (network_config_forward && received)
   {
-    /*c_complain_once (LOG_ERR, &complain_forwarding,
+    /*
+	c_complain_once (LOG_ERR, &complain_forwarding,
         "network plugin: A notification has been received via the network "
         "forwarding if enabled. Forwarding of notifications is currently "
         "not supported, because there is not loop-deteciton available. "
         "Please contact the collectd mailing list if you need this "
-        "feature.");*/
+        "feature.");
+	*/
 	return (1);
   }
 
